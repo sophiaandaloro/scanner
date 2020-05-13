e50cfbe17c647d213993737ba44eafdc6e7fd6d9
Sophia Andaloro <sja5@rice.edu>
2020-05-13 12:32:36 -0500

    Update to simplify and include a Register option
    Rehauled a lot of the numpy meshgrid stuff as well as introduced an option to pass register as an argument in scan_parameters. Thanks to Daniel's keen insight for making this work.

--- mystuff.py
+++ mystuff.py
@@ -1,37 +1,39 @@
+import sys 
+
 import numpy as np
 from collections import OrderedDict
-
 import scanner
 
 parameters = OrderedDict()
 
-# EDIT HERE
-run_id = '180215_1029'
-parameters['tail_veto_threshold']  = [1e4, 1e5],
-parameters['tail_veto_pass_fraction'] = [0.1, 0.2]
-
-# DO NOT EDIT BELOW
-
-keys = list(parameters.keys())
-values = list(parameters.values())
-
-combination_values = np.array(np.meshgrid(*values)).T.reshape(-1,
-                                                              len(parameters))
-
-strax_options = []
-for i, value in enumerate(combination_values):
-    print('Setting %d:' % i)
-    config = {}
-    for j, parameter in enumerate(value):
-          print('\t', keys[j], parameter)
-          config[keys[j]] = parameter
-    strax_options.append({'run_id' : run_id, 'config' : config})
-    print()
-
-print(strax_options)
-
-scanner.scan_parameters(strax_options)
+# IF YOU WANT TO REGISTER YOUR OWN PLUGIN FOR THE SCAN, USE THESE LINES. 
+# In case you want to register any non-standard plugins which is not part of the offical straxen 
+# you have to specify the path where the .py file with the plugin can be found:
+sys.path.append('/path/to/your/plugin/')
+# and import it.
+import MyPlugin # Or whatever your plugin is named. Skip if not doing plugin.
 
 
 
+target = ''
+name = 'me' # A short (<5 characters?) name to find your scanner among the masses of jobs. Initials are good.
+output_directory = './strax_data'
+# Plugins to be registered, can be None, single Plugin or a list of Plugins.
+register = HitCounting 
 
+#Specify here all of the config settings you want to look at. 
+paramter_dict = {'run_id': run_id, # can also be a list of run_ids. 
+                 'threshold': 15,
+                 'save_outside_hits_left': [10, 20, 30, 40],
+                 'save_outside_hits_right': [100, 120, 140, 160, 180, 200, 220, 240, 260]}
+
+#scan over everything in strax_options. scanner.py will enumerate over all that you've provided. 
+scanner.scan_parameters(target,
+                        paramter_dict,
+                        register=register,
+                        output_directory=output_directory,
+                        name=f'{name}_scan',
+                        job_config={'n_cpu': 2, 
+                                    'max_hours': 1},
+                        xenon1t=False
+                       )
