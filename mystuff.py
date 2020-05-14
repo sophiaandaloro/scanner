65240f51a6fc2dbc18e805d0c34b68269243e607
Sophia Andaloro <sja5@rice.edu>
2020-05-14 12:28:36 -0500

    update mystuff so that the example actually runs
    Before it was calling to a private dir in dali. This way it will just automatically register no new plugins, unless specified.

--- mystuff.py
+++ mystuff.py
@@ -1,39 +1,37 @@
-import sys 
-
-import numpy as np
-from collections import OrderedDict
 import scanner
+import sys
 
-parameters = OrderedDict()
-
-# IF YOU WANT TO REGISTER YOUR OWN PLUGIN FOR THE SCAN, USE THESE LINES. 
+import straxen
+### EDIT BELOW TO CHANGE CONFIG SETTINGS ###
 # In case you want to register any non-standard plugins which is not part of the offical straxen 
 # you have to specify the path where the .py file with the plugin can be found:
-sys.path.append('/path/to/your/plugin/')
+# sys.path.append('/home/dwenz/python_scripts/XENONnT/analysiscode/PMTs/HitFinder/Threshold')
 # and import it.
-import MyPlugin # Or whatever your plugin is named. Skip if not doing plugin.
+# from HitFinderThresholdPlugin import HitIntegratingAnalysis
 
 
-
-target = ''
-name = 'me' # A short (<5 characters?) name to find your scanner among the masses of jobs. Initials are good.
+# TODO: Change these parameters back to some default example...
+run_id = '007447' 
+target = 'led_calibration'# 'hitfinder_hits'
+name = 'hf' # TODO: I think Sophia mentioned a max number of characters. 
 output_directory = './strax_data'
-# Plugins to be registered, can be None, single Plugin or a list of Plugins.
-register = HitCounting 
-
-#Specify here all of the config settings you want to look at. 
-paramter_dict = {'run_id': run_id, # can also be a list of run_ids. 
-                 'threshold': 15,
-                 'save_outside_hits_left': [10, 20, 30, 40],
-                 'save_outside_hits_right': [100, 120, 140, 160, 180, 200, 220, 240, 260]}
-
-#scan over everything in strax_options. scanner.py will enumerate over all that you've provided. 
+# register=[HitIntegratingAnalysis] # Plugins to be registered, can be None, single Plugin or a list of Plugins.
+register=None
+paramter_dict = {'run_id': run_id, # can also be a list of run_ids, to apply our scan to multiple runs.
+                 'threshold': 50,
+                 'save_outside_hits_left': [10, 20],
+                 'save_outside_hits_right': [100]}
+paramter_dict = {'run_id': run_id, 
+# 		 'tail_veto_threshold': [1e5, 1e6],
+# 		}
+#scan over everything in strax_options
 scanner.scan_parameters(target,
                         paramter_dict,
                         register=register,
                         output_directory=output_directory,
                         name=f'{name}_scan',
-                        job_config={'n_cpu': 2, 
-                                    'max_hours': 1},
+                        job_config={'n_cpu': 10, 
+                                    'max_hours': 1,
+                                   'partition': 'xenon1t'},
                         xenon1t=False
                        )
