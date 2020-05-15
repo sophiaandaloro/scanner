94c2ac2a725a44c556e8f59ffed2cdebaa0a593a
Sophia Andaloro <sja5@rice.edu>
2020-05-15 14:47:48 -0500

    Update mystuff.py

--- mystuff.py
+++ mystuff.py
@@ -5,18 +5,19 @@ import straxen
 ### EDIT BELOW TO CHANGE CONFIG SETTINGS ###
 # In case you want to register any non-standard plugins which is not part of the offical straxen 
 # you have to specify the path where the .py file with the plugin can be found:
-sys.path.append('/path/to/plugin')
+# sys.path.append('/path/to/plugin')
 # and import it.
-from HitFinderThresholdPlugin import HitIntegratingAnalysis
-
+# from HitFinderThresholdPlugin import HitIntegratingAnalysis
+from led_calibration import LEDCalibration
 # Target to acquire with strax (should be a data kind)
-target = 'hitfinder_hits'
+target = 'led_calibration'# 'hitfinder_hits'
 # Plugins to be registered, can be None, single Plugin or a list of Plugins.
-register=[HitIntegratingAnalysis] 
-
+# register=[HitIntegratingAnalysis] 
+register=LEDCalibration
+# register=None
 # Add your initials so your scanner jobs can be found/uniquely ID'd 
 # m(keep characters <5 to not get thrown to resources queue)
-name = 'hf' 
+name = 'sja' 
 output_directory = './strax_data' #where the new processed data will be saved.
 
 # Here are some notes how you have to specifiy the parameter settings:
@@ -27,14 +28,16 @@ output_directory = './strax_data' #where the new processed data will be saved.
 #     combination for you. e.g. for {a: [1,2], b:[10, 20]} --> 1.: a=1, b=10, 2.: a=1, b=20, 3.: a=1, b=10, 4.: a=2, b=20
 # 3.) tuple, (round brackets () ) have a special meaning for the scanner with tuples
 #     we indicate settings which should not be terated e.g. if you have a setting
-#     like "search_window": (110, 140) or e.g. a left and right hit extension. You can 
+#     like "search_window":  and right hit e(110, 140) or e.g. a leftxtension. You can 
 #     also iterate over tuple-settings when specified as a list. E.g. if you want to 
-#     check multiple search windows you can do "search_window": [(110, 140), (128, 150), (134, 170)]
-paramter_dict = {'run_id': ['007447', '007455'], # can also be a list of run_ids, to apply our scan to multiple runs.
-                 'threshold': 15,
-                 'save_outside_hits_left': 20,
-                 'save_outside_hits_right': [100, 120]}
-
+#     check multiple search windows yoseu can do "arch_window": [(110, 140), (128, 150), (134, 170)]
+# paramter_dict = {'run_id': ['007447' , '007455'], # can also be a list of run_ids, to apply our scan to multiple runs.
+#                  'threshold': 15,
+#                  'save_outside_hits_left': 20,
+#                  'save_outside_hits_right': [100, 120]}
+paramter_dict = {'run_id': ['007447'],
+                 'baseline_window': [(0,30), (0,40)]
+                }
 #scan over everything in strax_options
 #Options here: 
 # n_cpu: How many CPUs per each setting to request. Try to limit number
